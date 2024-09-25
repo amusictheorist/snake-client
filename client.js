df4978c59c860b209925ffbbc29042bf3eaa3896
amusictheorist <a_millaresthomson@outlook.com>
2024-09-25 19:48:19 -0400

    Fixing some bugs

--- client.js
+++ client.js
@@ -1,17 +1,29 @@
 const net = require("net");
-// establishes a connection with the game server
-const connect = function () {
+
+// Establishes a connection with the game server
+const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541
   });
 
-  // interpret incoming data as text
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
-conn.on("data", (data) => {
-  console.log("Server says: ", data);
-})
+
+  // Log server messages
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  // Register handler for 'connect' event
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+
+    // Send name to server upon connection
+    conn.write("Name: AMT"); // Replace ABC with your chosen initials
+  });
+
   return conn;
 };
 
-module.exports = connect;
+module.exports = { connect };
