6aee7cf9167d608089b45110e7f7674ea062971d
amusictheorist <a_millaresthomson@outlook.com>
2024-09-25 18:30:05 -0400

    Fixed error, imported func

--- client.js
+++ client.js
@@ -1,8 +1,16 @@
+// establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541
   });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+conn.on("data", (data) => {
+  console.log("Server says: ", data);
+})
+  return conn;
 };
 
 module.exports = connect;
