2544dc6a843c1df71336815c6a0a99cdeb3f6f8c
amusictheorist <a_millaresthomson@outlook.com>
2024-09-25 21:11:00 -0400

    First commit, inserted constants

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = "localhost";
+const PORT = 50541;
+const MOVE_UP_KEY = "w";
+const MOVE_LEFT_KEY = "a";
+const MOVE_DOWN_KEY = "s";
+const MOVE_RIGHT_KEY = "d";
+const HUNGRY_MESSAGE = "h";
+const SSS_MESSAGE = "z";
+const PYTHON_MESSAGE = "p";
+const RATTLE_MESSAGE = "r";
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  HUNGRY_MESSAGE,
+  SSS_MESSAGE,
+  PYTHON_MESSAGE,
+  RATTLE_MESSAGE
+};
